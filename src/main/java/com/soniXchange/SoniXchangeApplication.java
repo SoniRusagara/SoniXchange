96f1a372cf30d378d8e5cc290aa1750635a403af
Soni Rusagara <soni.rusagara@gmail.com>
2025-08-02 19:53:07 -0400

    🚀 Initial commit: Setup Spring Boot project for SoniXchange

---
+++ src/main/java/com/soniXchange/SoniXchangeApplication.java
@@ -0,0 +1,13 @@
+package com.soniXchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoniXchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoniXchangeApplication.class, args);
+	}
+
+}

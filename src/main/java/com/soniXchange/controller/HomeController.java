2cd5c4891b14b83bd7d7f3cb9c67817a2e4788c3
Soni Rusagara <soni.rusagara@gmail.com>
2025-08-03 13:45:48 -0400

    Finalize SQL setup & create first API 8/3/2025

---
+++ src/main/java/com/soniXchange/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.soniXchange.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HomeController
+ *
+ * This controller provides a simple landing endpoint for
+ * the SoniXChange platform. Serves as the entry point
+ * to verify that the application is running successfully.
+ */
+@RestController
+public class HomeController {
+
+
+  /**
+   * Returns a welcome message for the SoniXChange platform.
+   *
+   * @return String - a user‑friendly welcome message.
+   */
+  @GetMapping
+  public String home(){
+    return "Welcome to SoniXChange Platform";
+  }
+}

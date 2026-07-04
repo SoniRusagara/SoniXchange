73c19e2ce6fea79f2dd4c28e6564dc2da6ce6e30
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-04 18:24:12 -0400

    Fix bug in TwoFactorAuth.java

--- src/main/java/com/soniXchange/model/TwoFactorAuth.java
+++ src/main/java/com/soniXchange/model/TwoFactorAuth.java
@@ -3,6 +3,7 @@ package com.soniXchange.model;
 import com.soniXchange.domain.VerificationType;
 
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import lombok.Data;
 
 /**

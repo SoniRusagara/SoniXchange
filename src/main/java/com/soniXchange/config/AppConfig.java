d039239a19c8b0a5d043ef458b4a03910d04e09b
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-07 16:01:03 -0400

    Fix missing import bug

--- src/main/java/com/soniXchange/config/AppConfig.java
+++ src/main/java/com/soniXchange/config/AppConfig.java
@@ -1,17 +1,33 @@
 package com.soniXchange.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll())
-       
+            .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+            .csrf(csrf->csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+
+        return http.build();
+    }
+
+    // Configure CORS to restrict API access to specific development domains
+    private CorsConfigurationSource corsConfigurationSource() {
+        return null;
+    }
+    
+}

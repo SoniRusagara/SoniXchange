bae729ca5ab5ca62d0ab27eb9000e383227416f6
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-08 09:45:50 -0400

    Build AuthResponse file

--- src/main/java/com/soniXchange/response/AuthResponse.java
+++ src/main/java/com/soniXchange/response/AuthResponse.java
@@ -1,5 +1,11 @@
 package com.soniXchange.response;
 
 public class AuthResponse {
+
+    private String jwt;
+    private boolean status;
+    private String message; 
+    private boolean isTwoFactorAuthEnabled;
+    private String session; 
     
 }

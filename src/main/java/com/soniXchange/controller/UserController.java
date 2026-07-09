dc3c414f81953fd6484a79c2fce1952a04459896
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-09 10:17:33 -0400

    Add UserController file

---
+++ src/main/java/com/soniXchange/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.soniXchange.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soniXchange.model.User;
+import com.soniXchange.service.EmailService;
+import com.soniXchange.service.UserService;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/api/users/profile")
+    public ResponseEntity<User> getUserProfile(@RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserProfileByJwt(jwt);
+
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @PatchMapping("/api/users/enable-two-factor/verify-otp.{otp}")
+    public ResponseEntity<User> enableTwoFactorAuthentication(@RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserProfileByJwt(jwt);
+
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+
+    
+}

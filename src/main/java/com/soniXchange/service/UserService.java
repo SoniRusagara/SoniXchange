6c99350d04c38f1f2cdce6699b39a80cbd389b20
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-09 10:11:13 -0400

    Add UserServiceImpl file

--- src/main/java/com/soniXchange/service/UserService.java
+++ src/main/java/com/soniXchange/service/UserService.java
@@ -1,14 +1,19 @@
 package com.soniXchange.service;
 
+import com.soniXchange.domain.VerificationType;
 import com.soniXchange.model.User;
 
 public interface UserService {
 
-    public User findUserProfileByJwt(String jwt);
-    public User findUserByEmail(String email);
-    public User findUserById(Long userId);
+    public User findUserProfileByJwt(String jwt) throws Exception;
+    public User findUserByEmail(String email) throws Exception;
+    public User findUserById(Long userId) throws Exception;
 
-    public User enableTwoFactorAuthentication(User user);
+    public User enableTwoFactorAuthentication(
+        VerificationType verificationType, 
+        String sendTo, 
+        User user
+    );
 
     User updatePassword(User user, String newPassword);
 

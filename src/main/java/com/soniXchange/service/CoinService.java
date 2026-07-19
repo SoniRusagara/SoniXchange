7c9816c3152760ff0c1104904628808721d52470
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-18 20:30:34 -0400

    Add logic to CoinServiceImpl

--- src/main/java/com/soniXchange/service/CoinService.java
+++ src/main/java/com/soniXchange/service/CoinService.java
@@ -7,15 +7,12 @@ import com.soniXchange.model.Coin;
 public interface CoinService {
     List<Coin> getCoinList(int page) throws Exception ;
 
-    String getMarketChart(String coinId, int days);
+    String getMarketChart(String coinId, int days) throws Exception;
 
-    String getCoinDetails(String coinId);
+    String getCoinDetails(String coinId) throws Exception;
 
     Coin findById(String coinId);
 
     String searchCoin(String keyword);
 
     String getTop50CoinsByMarketCapRank();
-
-    String getTradingCoins();
-}

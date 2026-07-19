af81662533db6c64c911efb60660196c2f635247
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-18 22:01:39 -0400

    Finish Coin Api logic

--- src/main/java/com/soniXchange/repository/CoinRepository.java
+++ src/main/java/com/soniXchange/repository/CoinRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soniXchange.model.Coin;
 
-public interface CoinRepository extends JpaRepository<Coin, Integer>{
-    
+public interface CoinRepository extends JpaRepository<Coin, String>{
+
     
 }
--- src/main/java/com/soniXchange/service/CoinServiceImpl.java
+++ src/main/java/com/soniXchange/service/CoinServiceImpl.java
@@ -1,6 +1,7 @@
 package com.soniXchange.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -80,60 +81,4 @@ public class CoinServiceImpl implements CoinService{
             
             HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-
-            JsonNode jsonNode = objectMapper.readTree(response.getBody());
-
-            Coin coin = new Coin();
-            coin.setId(jsonNode.get("id").asText());
-            coin.setName(jsonNode.get("name").asText());
-            coin.setSymbol(jsonNode.get("symbol").asText());
-            coin.setImage(jsonNode.get("image").get("large").asText());
-            
-            JsonNode marketData = jsonNode.get("market_data");
-
-            coin.setCurrentPrice(marketData.get("current_price").get("usd").asDouble());
-            coin.setMarketCap(marketData.get("market_cap").get("usd").asLong());
-            coin.setMarketCapRank(marketData.get("market_cap_rank").asInt());
-            coin.setTotalVolume(marketData.get("total_volume").get("usd").asLong());
-            coin.setHigh24h(marketData.get("high_24h").get("usd").asDouble());
-            coin.setLow24h(marketData.get("low_24h").get("usd").asDouble());
-            coin.setPriceChange24h(marketData.get("price_change2_4h").get("usd").asDouble());
-            coin.setPriceChangePercentage24h(marketData.get("price_change2_24h").get("usd").asDouble());
-            coin.setMarketCapChange24h(marketData.get("market_cap_change_24h").asLong());
-            coin.setMarketCapChangePercentage24h(marketData.get("market_cap_change_percentage").asDouble());
-            coin.setTotalSupply(marketData.get("total_supply").get("usd").asLong());
-            coinRepository.save(coin);
-
-            return response.getBody();
-
-        } catch (HttpClientErrorException | HttpServerErrorException e){
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    @Override
-    public Coin findById(String coinId) {
-        Optional<Coin> optionalCoin = coinRepository.findById(coinId);
-        
-    }
-
-    @Override
-    public String searchCoin(String keyword) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchCoin'");
-    }
-
-    @Override
-    public String getTop50CoinsByMarketCapRank() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTop50CoinsByMarketCapRank'");
-    }
-
-    @Override
-    public String getTradingCoins() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTradingCoins'");
-    }
-    
-}
+            ResponseEntit

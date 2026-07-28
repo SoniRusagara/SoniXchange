46eb142c319928c2f69870be2d9815a6fdc46895
Soni Rusagara <soni.rusagara@gmail.com>
2026-07-28 13:18:17 -0400

    Finish Watchlist controller

---
+++ src/main/java/com/soniXchange/controller/WatchlistController.java
@@ -0,0 +1,62 @@
+package com.soniXchange.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soniXchange.model.Coin;
+import com.soniXchange.model.User;
+import com.soniXchange.model.Watchlist;
+import com.soniXchange.service.CoinService;
+import com.soniXchange.service.UserService;
+import com.soniXchange.service.WatchlistService;
+
+@RestController
+@RequestMapping("/api/watchlist")
+public class WatchlistController {
+    
+    @Autowired
+    private WatchlistService watchlistService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CoinService coinService;
+
+    @GetMapping("/user")
+    public ResponseEntity<Watchlist> getUserWatchList(
+        @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Watchlist watchlist = watchlistService.findUserWatchlist(user.getId());
+        return ResponseEntity.ok(watchlist);
+    }
+
+    @GetMapping("/{watchlistId")
+    public ResponseEntity<Watchlist> getWatchListById(
+        @PathVariable Long watchlistId
+    ) throws Exception {
+        Watchlist watchlist = watchlistService.findById(watchlistId);
+        return ResponseEntity.ok(watchlist);
+    }
+
+    @PatchMapping("/add/coin/{coinId}")
+    public ResponseEntity<Coin> addItemToWatchList(
+        @RequestHeader("Authorization") String jwt,
+        @PathVariable String coinId
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        Coin coin = coinService.findById(coinId);
+        Coin addedCoin = watchlistService.addItemToWatchlist(coin, user);
+        return ResponseEntity.ok(addedCoin);
+    }
+
+}

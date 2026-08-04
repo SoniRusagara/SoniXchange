cf345bc442653af47b490e11d8ccaa404adae142
Soni Rusagara <soni.rusagara@gmail.com>
2026-08-04 13:44:23 -0400

    Finish PaymentController

---
+++ src/main/java/com/soniXchange/controller/PaymentController.java
@@ -0,0 +1,53 @@
+package com.soniXchange.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soniXchange.service.UserService;
+import com.stripe.exception.StripeException;
+import com.razorpay.RazorpayException;
+import com.soniXchange.domain.PaymentMethod;
+import com.soniXchange.model.PaymentOrder;
+import com.soniXchange.model.User;
+import com.soniXchange.response.PaymentResponse;
+import com.soniXchange.service.PaymentService;
+
+@RestController
+@RequestMapping("/api")
+public class PaymentController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/api/payment/{paymentMethod}/amount/{amount}")
+    public ResponseEntity<PaymentResponse> paymentHandler(
+        @PathVariable PaymentMethod paymentMethod,
+        @PathVariable Long amount, 
+        @RequestHeader("Authorization") String jwt) throws Exception, RazorpayException, StripeException {
+            
+            User user = userService.findUserProfileByJwt(jwt);
+
+            PaymentResponse paymentResponse; 
+
+            PaymentOrder order = paymentService.createOrder(user, amount, paymentMethod);
+
+            if(paymentMethod.equals(PaymentMethod.RAZORPAY)){
+                paymentResponse = paymentService.createRazorpayPaymentLink(user, amount);
+            } else {
+                // TODO: Verify 2nd param is not order.getAmount()
+                paymentResponse = paymentService.createStripePaymentLink(user, amount, order.getId());
+            }
+
+            return new ResponseEntity<>(paymentResponse, HttpStatus.CREATED);
+
+        }
+}
